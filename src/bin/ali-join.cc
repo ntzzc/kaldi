04f7ced80f7a382bef794a97a9decd6fc7238439
dengwei <dengwei.2011@gmail.com>
2017-02-17 17:41:26 +0800

    join alignment

--- src/bin/ali-join.cc
+++ src/bin/ali-join.cc
@@ -29,12 +29,12 @@ int main(int argc, char *argv[]) {
         "Join alignments [and possibly change format]\n"
         "Usage: ali-join <align-in-rspecifier1> [<align-in-rspecifier2>"
         " <align-in-rspecifier3> ...] <align-out-wspecifier>\n"
-    	"  e.g.: ali-join ark:ali1.ark ark:ali2.ark ark:join.ark\n";
+    	"  e.g.: ali-join --ali-dim=\"10092:411\" ark:ali1.ark ark:ali2.ark ark:join.ark\n";
 
     ParseOptions po(usage);
 
-    bool add_shift = true;
-    po.Register("add-shift", &add_shift, "Add the alignment target shift.");
+    std::string ali_dim_str;
+    po.Register("ali-dim", &ali_dim_str, "Each input alignment dim.");
 
     po.Read(argc, argv);
 
@@ -42,6 +42,8 @@ int main(int argc, char *argv[]) {
       po.PrintUsage();
       exit(1);
     }
+
+    
 
     int32 num_args = po.NumArgs();
     std::string ali_in_fn1 = po.GetArg(1),
@@ -55,31 +57,51 @@ int main(int argc, char *argv[]) {
     std::vector<RandomAccessInt32VectorReader*> ali_readers(num_args-2,
                        static_cast<RandomAccessInt32VectorReader*>(NULL));
 
-    for (int32 i = 2; i < num_args; ++i)
+    std::vector<std::string> ali_in_fns(num_args-2);
+    for (int32 i = 2; i < num_args; ++i) {
     	ali_readers[i-2] = new RandomAccessInt32VectorReader(po.GetArg(i));
+        ali_in_fns[i-2] = po.GetArg(i);
+    }
 
+    std::vector<int32> ali_dim(num_args-1);
+    if (!kaldi::SplitStringToIntegers(ali_dim_str, ":", false, &ali_dim))
+        KALDI_ERR << "Invalid alignment dim string " << ali_dim_str;
 
-    int32 shift, num_done = 0;
+    std::vector<int32> ali_dim_offset(num_args-1, 0);
+    for (int32 i = 1; i < num_args-1; i++)
+        ali_dim_offset[i] = ali_dim_offset[i-1] + ali_dim[i-1];
+
+    int32 num_done = 0, num_missing = 0, i;
     for (; !ali_reader1.Done(); ali_reader1.Next()) {
         std::string key = ali_reader1.Key();
         std::vector<int32> ali1 = ali_reader1.Value();
-        ali_reader1.FreeCurrent();
+        //ali_reader1.FreeCurrent();
 
-        for (int32 i = 0; i < num_args-2; ++i) {
+        for (i = 0; i < num_args-2; ++i) {
              if (ali_readers[i]->HasKey(key)) {
-               std::vector<int32> ali2 = ali_readers[i].Value(key);
+               std::vector<int32> ali2 = ali_readers[i]->Value(key);
 
-               shift = add_shift ? ali1.size() : 0;
                for (int32 j = 0; j < ali2.size(); j++)
-            	   ali1.push_back(ali2[j] + shift);
+            	   ali1.push_back(ali2[j] + ali_dim_offset[i+1]);
+           } else {
+                KALDI_WARN << "No alignment found for utterance " << key << " for "
+                   << "system " << (i + 2) << ", rspecifier: "
+                   << ali_in_fns[i];
+                num_missing++;
+                break;
            }
         }
+        
+        if (i < num_args-2) 
+            continue;
 
         ali_writer.Write(key, ali1);
-
-        KALDI_LOG << "Copied " << num_done << " alignment utterances.";
-        return (num_done != 0 ? 0 : 1);
+        num_done++;
     }
+
+    KALDI_LOG << "Processed " << num_done << " alignment utterances successful. "
+                << num_missing << " total missing utterances";
+    return (num_done != 0 ? 0 : 1);
   } catch(const std::exception &e) {
     std::cerr << e.what();
     return -1;
